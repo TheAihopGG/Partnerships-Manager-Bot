1150d019641a7a3ab7cd90235be1df937ef23a55
theaihopgg <theaihopgg@gmail.com>
2025-10-15 21:57:18 +0700

    s

--- pmbot/core/config.py
+++ pmbot/core/config.py
@@ -7,14 +7,16 @@ basicConfig()
 DEV_MODE: bool = bool(int(assert_getenv("DEV_MODE")))
 SQLALCHEMY_URL: str = (
     f"\
-    postgressql+psycopg://\
-    {assert_getenv("POSTGRES_USER")}:{assert_getenv("POSTGRES_PASSWORD")}\
-    @\
-    {assert_getenv("POSTGRES_IP")}:{assert_getenv("POSTGRES_PORT")}\
-    /\
-    {assert_getenv("POSTGRES_DB")}"
+postgresql+psycopg://\
+{assert_getenv("POSTGRES_USER")}:{assert_getenv("POSTGRES_PASSWORD")}\
+@\
+{assert_getenv("POSTGRES_IP")}:{assert_getenv("POSTGRES_PORT")}\
+/\
+{assert_getenv("POSTGRES_DB")}"
 )
 NATS_URL: str = f"http://{assert_getenv("NATS_IP")}:{assert_getenv("NATS_PORT")}"
-TEST_GUILDS: list[int] = [int(id) for id in assert_getenv("TEST_GUILDS").rstrip().split(",")]
+TEST_GUILDS: list[int] = [
+    int(id) if id else None for id in assert_getenv("TEST_GUILDS").rstrip().split(",")
+]
 
 logger.info("PMBot config loaded")
--- pmbot/core/database.py
+++ pmbot/core/database.py
@@ -1,5 +1,8 @@
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from logging import getLogger
+
 from .config import SQLALCHEMY_URL
 
+logger = getLogger(__name__)
 engine = create_async_engine(SQLALCHEMY_URL)
-sessionmaker = async_sessionmaker(engine)
+sessionmaker = async_sessionmaker(engine, autoflush=True)
