43fb5c3dec449c34eca3802cfa5d580ac5791a47
theaihopgg <theaihopgg@gmail.com>
2025-10-15 21:46:08 +0700

    s

--- pmbot/main.py
+++ pmbot/main.py
@@ -2,9 +2,18 @@ from disnake.ext.commands import InteractionBot
 from disnake import Intents
 
 from core import TEST_GUILDS, DEV_MODE, assert_getenv
+from cogs import help, partnerships, settings
 
 bot = InteractionBot(
     intents=Intents.default(),
     test_guilds=TEST_GUILDS if DEV_MODE else None,
 )
+[
+    bot.add_cog(cog)
+    for cog in {
+        help.HelpCog(),
+        partnerships.PartnershipsCog(),
+        settings.SettingsCog(),
+    }
+]
 bot.run(assert_getenv("BOT_TOKEN"))
