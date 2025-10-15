b4fcff68fd0f4d05595fa15e9d783f1cb6c197ba
theaihopgg <theaihopgg@gmail.com>
2025-10-15 20:54:47 +0700

    c

--- pmbot/core/config.py
+++ pmbot/core/config.py
@@ -5,12 +5,19 @@ logger = getLogger(__name__)
 basicConfig()
 
 DEV_MODE: bool = bool(int(assert_getenv("DEV_MODE")))
-SQLALCHEMY_URL: str = f"\
-postgressql+psycopg://\
-{assert_getenv("POSTGRES_USER")}:{assert_getenv("POSTGRES_PASSWORD")}\
-@\
-{assert_getenv("POSTGRES_IP")}:{assert_getenv("POSTGRES_PORT")}\
-/\
-{assert_getenv("POSTGRES_DB")}"
+SQLALCHEMY_URL: str = (
+    f"\
+    postgressql+psycopg://\
+    {assert_getenv("POSTGRES_USER")}:{assert_getenv("POSTGRES_PASSWORD")}\
+    @\
+    {assert_getenv("POSTGRES_IP")}:{assert_getenv("POSTGRES_PORT")}\
+    /\
+    {assert_getenv("POSTGRES_DB")}"
+)
 
 logger.info("PMBot config loaded")
+
+__all__ = (
+    "DEV_MODE",
+    "SQLALCHEMY_URL",
+)
